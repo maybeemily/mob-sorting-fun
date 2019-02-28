016118cc3fd23c24ec6dc1d38ba0ec2ea9d14080
emily baier <emilyrbaier@gmail.com>
2019-02-28 14:20:55 -0800

    added a really rad drop shadow on hover

--- src/pokemon-template.js
+++ src/pokemon-template.js
@@ -3,7 +3,7 @@ export default function makePokemonCard(pokemon) {
     <li style="background-color:${pokemon.color_1}">
         <h2>${pokemon.pokemon} <span class="type_2" style="background:${pokemon.color_2}"></span></h2>
         <img src="${pokemon.url_image}">
-        <p class="bold">
+        <p class="stats">
             <span>HP: ${pokemon.hp}</span> <span>A: ${pokemon.attack}</span> <span>D: ${pokemon.defense}</span>
         </p>
     </li>`;
