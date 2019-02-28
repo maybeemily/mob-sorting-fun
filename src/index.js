39df94c5e51b3c76ebbed3428e555123970441b3
emily baier <emilyrbaier@gmail.com>
2019-02-28 13:35:42 -0800

    styling changes, fixed variable error

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import data from '../data/pokedex.js';
 import loadPokemon from './load-pokemon.js';
 
-const shortList = data.slice(0, 3);
+//const shortList = data.slice(0, 3);
 
-loadPokemon(shortList);
+loadPokemon(data);
 
