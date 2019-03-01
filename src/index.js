482eb9e702cee40cb907aaf08d8063a7101533c4
emily baier <emilyrbaier@gmail.com>
2019-02-28 16:06:44 -0800

    the pagination works now

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import data from '../data/pokedex.js';
 import loadPokemon from './load-pokemon.js';
 import loadPaging from './paging-component.js';
+import pageArray from './page-array.js';
 
 //const shortList = data.slice(0, 3);
 
 loadPokemon(data);
 
 loadPaging(data.length, pagingOptions => {
-    console.log(pagingOptions);
+    const pagedPokedex = pageArray(data, pagingOptions);
+    loadPokemon(pagedPokedex);
 });
--- src/load-pokemon.js
+++ src/load-pokemon.js
@@ -3,10 +3,18 @@ import makePokemonCard from './pokemon-template.js';
 const pokemonGallery = document.getElementById('pokemon-gallery');
 
 function loadPokemon(pokemon) {
+    clearPokedex();
+
     pokemon.forEach(pokemon => {
         const dom = makePokemonCard(pokemon);
         pokemonGallery.appendChild(dom);
     });
+
+    function clearPokedex() {
+        while(pokemonGallery.children.length > 0) {
+            pokemonGallery.lastElementChild.remove();
+        }
+    }
 }
 
 export default loadPokemon;
